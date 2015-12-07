b2f597bc3cc92870c0f63344be8dd6e2af787628
Nicole <sennewsonn@gmail.com>
2015-12-07 14:45:26 +0300

    обнова 0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 var gulp = require("gulp"),
-<<<<<<< HEAD
 browserSync = require('browser-sync');
 
 gulp.task('server', function() {
@@ -17,7 +16,6 @@ gulp.watch([
 'app/js/**/*.js',
 'app/css/**/*.css'
 ]).on('change', browserSync.reload);
-=======
 	browserSync = require('browser-sync');
 
 	gulp.task('server', function() {
@@ -35,7 +33,7 @@ gulp.task('watch', function () {
 		'app/js/**/*.js',
 		'app/css/**/*.css'
 	]).on('change', browserSync.reload);
->>>>>>> 7d62c72cebd1539443546865cbaffe24bd870d8c
+
 });
 
 gulp.task('default', ['server', 'watch']);
